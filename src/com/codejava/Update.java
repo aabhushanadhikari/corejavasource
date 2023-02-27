8f4b076c89b49b110930902600194e2a5af11159
aabhushanadhikari <aaabhushan10@gmail.com>
2023-02-27 09:22:35 +0545

    crud added

--- src/com/codejava/Update.java
+++ src/com/codejava/Update.java
@@ -2,34 +2,22 @@ package com.codejava;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class UsersManager{
+public class Update {
 	public static void main(String[] args) {
 		String jdbcURL="jdbc:mysql://localhost:3306/sampledb";
 		String dbusername="root";
 		String dbpassword="";
-		String username="Aabhushan";
-		String password="password";
-		String email="aaabhushan@gmail.com";
-		String fullname="Aabhushan Adhikari";
-		
 		try {
-			System.out.println("Username"+username);
 			Connection connection=DriverManager.getConnection(jdbcURL,dbusername,dbpassword);
-			String sql=("INSERT INTO users (username,password,fullname,email)"+" VALUES ('Aabhushan', 'Adhikari', 'aaabhushan10@gmail.com', '12345')");
+			String sql="UPDATE users SET password ='keysk' WHERE username='Aabhushan'";
 			Statement stm=connection.createStatement();
-			
 			int rows=stm.executeUpdate(sql);
-		
 			if(rows>0) {
-				System.out.println("New Row added");
+				System.out.println("The table has been updated");
 			}
-			
-				connection.close();
-			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
